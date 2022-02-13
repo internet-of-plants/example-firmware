eb2ebe43753a0fad7f8f1be3f037e65dc786a340
Paulo Cabral Sanz <paulosanz@poli.ufrj.br>
2022-02-13 10:17:41 -0300

    Minor fix on NOOP driver

--- src/driver/runtime.cpp
+++ src/driver/runtime.cpp
@@ -5,8 +5,11 @@
 #elif defined(IOP_ESP8266)
 #include "driver/esp8266/runtime.hpp"
 #elif defined(IOP_NOOP)
-#include "driver/cpp17/runtime.hpp"
+#ifdef ARDUINO
 #include "driver/esp8266/runtime.hpp"
+#else
+#include "driver/cpp17/runtime.hpp"
+#endif
 #elif defined(IOP_ESP32)
 #include "driver/esp32/runtime.hpp"
 #else
