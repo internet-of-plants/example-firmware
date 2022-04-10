db175b83401272c70c1cb9def6725baee92b0c84
Paulo Cabral Sanz <paulosanz@poli.ufrj.br>
2022-04-10 00:38:47 -0300

    Remove config file

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,23 @@
 #include <iop/loop.hpp>
+#include <iop-hal/io.hpp>
 
 #include <dallas_temperature.hpp>
 #include <dht.hpp>
 #include <soil_resistivity.hpp>
 #include <factory_reset_button.hpp>
 
-#include "configuration.hpp"
+namespace config {
+constexpr static iop::time::milliseconds measurementsInterval = 180 * 1000;
 
-constexpr const uint32_t interval = 180 * 1000;
+constexpr static iop_hal::io::Pin soilTemperature = iop_hal::io::Pin::D5;
+constexpr static iop_hal::io::Pin airTempAndHumidity = iop_hal::io::Pin::D6;
+constexpr static iop_hal::io::Pin soilResistivityPower = iop_hal::io::Pin::D7;
+constexpr static iop_hal::io::Pin factoryResetButton = iop_hal::io::Pin::D1;
+
+/// Version of DHT (Digital Humidity and Temperature) sensor. (ex: DHT11 or DHT21 or DHT22...)
+constexpr static uint8_t dhtVersion = 22; // DHT22
+}
+
 
 sensor::SoilResistivity soilResistivity(config::soilResistivityPower);
 dallas::TemperatureCollection soilTemperature(config::soilTemperature);
@@ -22,7 +32,7 @@ auto setup(iop::EventLoop &loop) noexcept -> void {
   soilTemperature.begin();
   airTempAndHumidity.begin();
 
-  loop.setAuthenticatedInterval(interval, reportMeasurements);
+  loop.setAuthenticatedInterval(config::measurementsInterval, reportMeasurements);
   loop.setInterval(1000, reset::resetIfNeeded);
 }
 
