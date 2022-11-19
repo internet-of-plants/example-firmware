8cd3c20e0a2b9637192e664dcd4ba1a85e2c0158
Paulo Cabral Sanz <paulosanz@poli.ufrj.br>
2022-11-19 07:02:42 -0300

    Add light

--- src/main.cpp
+++ src/main.cpp
@@ -3,29 +3,40 @@
 #include <dht.hpp>
 #include <factory_reset_button.hpp>
 #include <soil_resistivity.hpp>
+#include <light.hpp>
 
 #include "pin.hpp"
 #include "generated/psk.hpp"
 
 namespace config {
 constexpr static iop::time::milliseconds measurementsInterval = 180 * 1000;
-constexpr static char SSID_RAW[] IOP_ROM = "iop";
+constexpr static iop::time::milliseconds unauthenticatedActionsInterval = 1000;
+constexpr static int8_t timezone = -3;
+
+static char SSID_RAW[] IOP_ROM = "iop";
 static const iop::StaticString SSID = reinterpret_cast<const __FlashStringHelper*>(SSID_RAW);
 static const iop::StaticString PSK = reinterpret_cast<const __FlashStringHelper*>(generated::PSK);
 
-constexpr static Pin airTempAndHumidity = Pin::D6;
-constexpr static dht::Version dhtVersion = dht::Version::DHT22;
-constexpr static Pin factoryResetButton = Pin::D1;
-constexpr static Pin soilResistivityPower = Pin::D7;
-constexpr static Pin soilTemperature = Pin::D5;
+static const Pin light = Pin::D4;
+static const std::pair<relay::Moment, relay::State> lightMoments[] = {
+  std::make_pair(relay::Moment(6, 0, 0), relay::State::ON),
+  std::make_pair(relay::Moment(0, 0, 0), relay::State::OFF),
+};
+static const Pin airTempAndHumidity = Pin::D6;
+static const dht::Version dhtVersion = dht::Version::DHT22;
+static const Pin factoryResetButton = Pin::D1;
+static const Pin soilResistivityPower = Pin::D7;
+static const Pin soilTemperature = Pin::D5;
 }
 
+static relay::Light light(IOP_PIN_RAW(config::light));
 static dallas::TemperatureCollection soilTemperature(IOP_PIN_RAW(config::soilTemperature));
 static dht::Dht airTempAndHumidity(IOP_PIN_RAW(config::airTempAndHumidity), config::dhtVersion);
 static sensor::SoilResistivity soilResistivity(IOP_PIN_RAW(config::soilResistivityPower));
 
 auto prepareJson(iop::EventLoop & loop) noexcept -> std::unique_ptr<iop::Api::Json> {
   IOP_TRACE();
+
   loop.logger().infoln(IOP_STR("Handle Measurements"));
   auto json = loop.api().makeJson(IOP_FUNC, [](JsonDocument &doc) {
     doc["air_temperature_celsius"] = airTempAndHumidity.measureTemperature();
@@ -33,7 +44,7 @@ auto prepareJson(iop::EventLoop & loop) noexcept -> std::unique_ptr<iop::Api::Js
     doc["soil_resistivity_raw"] = soilResistivity.measure();
     doc["soil_temperature_celsius"] = soilTemperature.measure();
   });
-  iop_assert(json, IOP_STR("Unable to send measurements, OOM or buffer overflow"));
+  iop_assert(json, IOP_STR("Unable to generate request payload, OOM or buffer overflow"));
   return json;
 }
 
@@ -41,14 +52,29 @@ auto reportMeasurements(iop::EventLoop &loop, const iop::AuthToken &token) noexc
   loop.registerEvent(token, *prepareJson(loop));
 }
 
+auto cleanup() noexcept -> void {
+  light.reset();
+}
+
+auto unauthenticatedAct(iop::EventLoop &loop) noexcept -> void {
+  reset::resetIfNeeded(loop);
+  light.actIfNeeded();
+}
+
 namespace iop {
 auto setup(EventLoop &loop) noexcept -> void {
   loop.setAccessPointCredentials(config::SSID, config::PSK);
+  for (const auto [moment, state]: config::lightMoments) {
+    light.setTime(moment, state);
+  }
   airTempAndHumidity.begin();
-  loop.setInterval(1000, reset::resetIfNeeded);
   reset::setup(IOP_PIN_RAW(config::factoryResetButton));
   soilResistivity.begin();
   soilTemperature.begin();
+
+  loop.setCleanup(cleanup);
+  loop.setTimezone(config::timezone);
+  loop.setInterval(config::unauthenticatedActionsInterval, unauthenticatedAct);
   loop.setAuthenticatedInterval(config::measurementsInterval, reportMeasurements);
 }
 }
